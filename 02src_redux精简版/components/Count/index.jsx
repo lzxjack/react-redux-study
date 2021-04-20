e9d7fe7bd037cef42e9f099fdaa24423d9d58ba6
Jack <965555169@qq.com>
2021-04-20 09:17:20 +0800

    redux_mini

---
+++ 02src_redux精简版/components/Count/index.jsx
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+// 引入store，获取redux中保存的状态
+import store from '../../redux/store';
+
+export default class Count extends Component {
+    // state = { count: 0 };
+    // componentDidMount() {
+    //     // 监测redux中状态的变化，只要变化，调用render
+    //     store.subscribe(() => {
+    //         this.setState({});
+    //     });
+    // }
+    // 加法
+    increment = () => {
+        const { value } = this.selectNumber;
+        store.dispatch({ type: 'increment', data: value * 1 });
+    };
+    // 减法
+    decrement = () => {
+        const { value } = this.selectNumber;
+        store.dispatch({ type: 'decrement', data: value * 1 });
+    };
+    // 奇数再加
+    incrementIfOdd = () => {
+        const { value } = this.selectNumber;
+        const count = store.getState();
+        if (count % 2 !== 0) {
+            store.dispatch({ type: 'increment', data: value * 1 });
+        }
+    };
+    // 异步加
+    incrementAsync = () => {
+        const { value } = this.selectNumber;
+        setTimeout(() => {
+            store.dispatch({ type: 'increment', data: value * 1 });
+        }, 500);
+    };
+    render() {
+        return (
+            <div>
+                <h1>当前求和为：{store.getState()}</h1>
+                <select ref={currentNode => (this.selectNumber = currentNode)}>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                </select>
+                &nbsp;&nbsp;
+                <button onClick={this.increment}>+</button>&nbsp;&nbsp;
+                <button onClick={this.decrement}>-</button>&nbsp;&nbsp;
+                <button onClick={this.incrementIfOdd}>当前求和为奇数再加</button>&nbsp;&nbsp;
+                <button onClick={this.incrementAsync}>异步加</button>
+            </div>
+        );
+    }
+}
---
+++ 02src_redux精简版/redux/count_reducer.js
@@ -0,0 +1,23 @@
+// 该文件创建一个为Count组件服务的reducer，reducer本质是一个函数
+// reducer函数接收两个参数，之前的状态preState，动作对象action
+
+// 设定初始化状态为0.
+const initState = 0;
+
+// 若preState为undefined，则传入initState
+export default function countReducer(preState = initState, action) {
+    // console.log(preState, action);
+    if (preState === undefined) preState = 0;
+    // 从action对象中获取type，data
+    const { type, data } = action;
+    // 根据type决定如何加工数据
+    switch (type) {
+        case 'increment':
+            return preState + data;
+        case 'decrement':
+            return preState - data;
+        // 初始化动作
+        default:
+            return preState;
+    }
+}
