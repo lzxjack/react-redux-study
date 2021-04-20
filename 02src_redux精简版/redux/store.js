75a2175591a071cdaf273bc693527e21682be0ad
Jack <965555169@qq.com>
2021-04-20 09:16:03 +0800

    redux_mini

--- 02src_redux精简版/redux/store.js
+++
@@ -1,8 +0,0 @@
-// 该文件专门用于暴露一个store对象，真个文档只有一个store对象
-
-// 引入createStore
-import { createStore } from 'redux';
-// 引入为Count组件服务的reducer
-import countReducer from './count_reducer';
-// 暴露store
-export default createStore(countReducer);
